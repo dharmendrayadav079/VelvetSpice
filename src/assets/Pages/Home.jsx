81896a159aefac0590dac83e4906497afe1d9af3
Dharmendra Yadav <dharmendrayadav07093@gmail.com>
2025-07-08 22:20:08 +0530

    fix: correct import path for Cart component

--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../../Component/Navbar'
 import CategoryMenu from '../../Component/CategoryMenu';
 import FoodItems from '../../Component/FoodItems';
-import Cart from '../../Component/cart';
+import Cart from '../../Component/Cart';
 
 const Home = () => {
   return(
